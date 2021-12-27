0e687a0224016a9ce2c3d328a17a559a249b2c05
Iqbal Sigma M <iqbalsigma@gmail.com>
2021-12-27 23:29:30 +0700

    Add files via upload

---
+++ BubbleSortDesc.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+//Created by : Iqbal Sigma
+
+class BubbleSortDesc {
+  public static void main(String []args) {
+    int angka, i, j, temp;
+    Scanner input = new Scanner(System.in);
+ 	System.out.println(""); 
+	System.out.println(""); 
+	System.out.println("=======BUBBLE SORT (DESC)======="); 
+    System.out.print("Masukkan jumlah angka yang ingin di urutkan: "); 
+    angka = input.nextInt();
+ 
+    int array[] = new int[angka];
+ 
+    System.out.println("Masukkan " + angka + " angka: ");
+ 
+    for (i = 0; i < angka; i++) 
+      array[i] = input.nextInt();
+ 
+    for (i = 0; i < ( angka - 1 ); i++) {
+      for (j = 0; j < angka - i - 1; j++) {
+        if (array[j] < array[j+1]) 
+        {
+          temp = array[j];
+          array[j] = array[j+1];
+          array[j+1] = temp;
+        }
+      }
+    }
+ 
+    System.out.println("Hasil Pengurutan Angka:");
+ 
+    for (i = 0; i < angka; i++) 
+      System.out.println(array[i]);
+  } 
+}
+
+
+
